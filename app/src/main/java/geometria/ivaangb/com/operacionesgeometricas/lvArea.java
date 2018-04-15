83bf46b134a7f4cb5640b183c2497aca1cba6231
IvaanGB <igonzale4@cuc.edu.co>
2018-04-15 17:05:24 -0500

    Conectados todos los intent, pendiente traducción y métodos para cada función

--- app/src/main/java/geometria/ivaangb/com/operacionesgeometricas/lvArea.java
+++ app/src/main/java/geometria/ivaangb/com/operacionesgeometricas/lvArea.java
@@ -1,13 +1,60 @@
 package geometria.ivaangb.com.operacionesgeometricas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class lvArea extends AppCompatActivity {
+
+    private ListView lArea;
+    private String [] opAreas;
+    private Intent in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lv_area);
+
+        lArea = findViewById(R.id.lvArea);
+        opAreas = getResources().getStringArray(R.array.areas);
+        ArrayAdapter <String> adapterArea = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1, opAreas);
+
+        lArea.setAdapter(adapterArea);
+        lArea.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        in = new Intent(lvArea.this, areaCuadrado.class);
+                        startActivity(in);
+                        break;
+
+                    case 1:
+                        in = new Intent(lvArea.this, areaRectangulo.class);
+                        startActivity(in);
+                        break;
+
+                    case 2:
+                        in = new Intent(lvArea.this, areaTriang.class);
+                        startActivity(in);
+                        break;
+
+                    case 3:
+                        in = new Intent(lvArea.this, areaCirculo.class);
+                        startActivity(in);
+                        break;
+                }
+            }
+        });
+
     }
+
+
+
+
 }
--- app/src/main/java/geometria/ivaangb/com/operacionesgeometricas/lvVolumen.java
+++ app/src/main/java/geometria/ivaangb/com/operacionesgeometricas/lvVolumen.java
@@ -1,13 +1,55 @@
 package geometria.ivaangb.com.operacionesgeometricas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class lvVolumen extends AppCompatActivity {
+    private ListView lVolumen;
+    private String [] opVolumen;
+    private Intent in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lv_volumen);
+
+        lVolumen = findViewById(R.id.lvVolumen);
+        opVolumen = getResources().getStringArray(R.array.volumenes);
+        ArrayAdapter<String> adapterArea = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1, opVolumen);
+
+        lVolumen.setAdapter(adapterArea);
+        lVolumen.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        in = new Intent(lvVolumen.this, volEsfera.class);
+                        startActivity(in);
+                        break;
+
+                    case 1:
+                        in = new Intent(lvVolumen.this, volCilindro.class);
+                        startActivity(in);
+                        break;
+
+                    case 2:
+                        in = new Intent(lvVolumen.this, volCono.class);
+                        startActivity(in);
+                        break;
+
+                    case 3:
+                        in = new Intent(lvVolumen.this, volCubo.class);
+                        startActivity(in);
+                        break;
+                }
+            }
+        });
     }
 }
+
