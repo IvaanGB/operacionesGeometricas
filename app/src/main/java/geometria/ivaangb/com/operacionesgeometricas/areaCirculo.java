f2c8cddb9cf9eb62c447c1b7adeefb7d10507334
IvaanGB <igonzale4@cuc.edu.co>
2018-04-15 02:19:56 -0500

    Interfaz gráfica terminada, faltantes: Métodos funcionales, traducción, enlace de actividades con Intent

---
+++ app/src/main/java/geometria/ivaangb/com/operacionesgeometricas/areaCirculo.java
@@ -0,0 +1,13 @@
+package geometria.ivaangb.com.operacionesgeometricas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class areaCirculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_area_circulo);
+    }
+}
---
+++ app/src/main/java/geometria/ivaangb/com/operacionesgeometricas/volCubo.java
@@ -0,0 +1,13 @@
+package geometria.ivaangb.com.operacionesgeometricas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class volCubo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vol_cubo);
+    }
+}
